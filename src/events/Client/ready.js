c170a86848dd7516e8743371d716ac547169b55e
Katsuyu <antoniusdarren07@gmail.com>
2023-05-26 16:56:32 +0700

    Update ready event

--- src/events/Client/ready.js
+++ src/events/Client/ready.js
@@ -1,4 +1,5 @@
 const { Client } = require("discord.js");
+const { connect } = require("mongoose");
 const { loadCommands } = require("../../structures/handlers/commandHandler");
 
 module.exports = {
@@ -12,5 +13,32 @@ module.exports = {
     bot.user.setActivity(`with ${bot.guilds.cache.size} guilds`);
 
     loadCommands(bot);
+
+    connect(bot.config.database, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+      .then(() =>
+        bot.logger.log(`Connected to the Mongodb database.`, ["CLIENT"])
+      )
+      .catch((err) => {
+        bot.logger.log(
+          "Unable to connect to the Mongodb database. Error:" + err,
+          ["CLIENT"]
+        );
+      });
+
+    bot
+      .on("disconnect", () =>
+        bot.logger.log("Bot is disconnecting...", ["WARN"])
+      )
+      .on("reconnecting", () =>
+        bot.logger.log("Bot reconnecting...", ["CLIENT"])
+      )
+      .on("error", (e) => bot.logger.log(e, ["ERROR"]))
+      .on("warn", (info) => bot.logger.log(info, ["WARN"]));
+    process.on("unhandledRejection", (err) => {
+      console.error(err);
+    });
   },
 };
