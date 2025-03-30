720b6d6fd4a3cbe50ae67371d57a56d98590308b
EX <antoniusdarren07@gmail.com>
2025-03-30 12:15:22 +0700

    alpha.12 - Fix

--- src/base/classes/Handler.ts
+++ src/base/classes/Handler.ts
@@ -7,67 +7,83 @@ import Command from "./Command";
 import SubCommand from "./SubCommand";
 
 export default class Handler implements IHandler {
-    client: ShizuClient;
-    constructor(client: ShizuClient){
-        this.client = client
+  client: ShizuClient;
+  constructor(client: ShizuClient) {
+    this.client = client;
+  }
+
+  async LoadEvents(): Promise<void> {
+    const files: string[] = (await glob("dist/events/**/*.js")).map((file) =>
+      path.resolve(file)
+    );
+
+    await Promise.all(
+      files.map(async (file: string) => {
+        try {
+          const e: Event = new (await import(file)).default(this.client);
+          this.registerEvent(e, file);
+        } catch (error: any) {
+          this.client.logger.error(
+            `Failed to load event from file ${file}: ${error.message}`,
+            ["Handler", "LoadEvents"]
+          );
+        }
+      })
+    );
+  }
+
+  private registerEvent(event: Event, file: string): void {
+    if (!event.name) {
+      delete require.cache[require.resolve(file)];
+      console.log(`${file.split("/").pop()} doesn't have a name`);
+      return;
     }
 
-    async LoadEvents(): Promise<void> {
-        const files: string[] = (await glob('build/events/**/*.js')).map(file => path.resolve(file));
-        
-        await Promise.all(files.map(async (file: string) => {
-            try {
-                const e: Event = new (await import(file)).default(this.client);
-                this.registerEvent(e, file);
-            } catch (error: any) {
-                this.client.logger.error(`Failed to load event from file ${file}: ${error.message}`, ["Handler", "LoadEvents"]);
-            }
-        }));
-    }
-    
-    private registerEvent(event: Event, file: string): void {
-        if (!event.name) {
-            delete require.cache[require.resolve(file)];
-            console.log(`${file.split('/').pop()} doesn't have a name`);
-            return;
+    const execute = (...args: any[]) => event.Execute(...args);
+
+    //@ts-ignore
+    if (event.once) this.client.once(event.name, execute);
+    //@ts-ignore
+    else this.client.on(event.name, execute);
+
+    delete require.cache[require.resolve(file)];
+  }
+
+  async LoadCommands(): Promise<void> {
+    const files: string[] = (await glob("dist/commands/**/*.js")).map((file) =>
+      path.resolve(file)
+    );
+
+    await Promise.all(
+      files.map(async (file: string) => {
+        try {
+          const c: Command | SubCommand = new (await import(file)).default(
+            this.client
+          );
+          this.registerCommand(c, file);
+        } catch (error: any) {
+          this.client.logger.error(
+            `Failed to load command from file ${file}: ${error.message}`,
+            ["Handler", "LoadCommands"]
+          );
         }
-    
-        const execute = (...args: any[]) => event.Execute(...args);
-    
-        //@ts-ignore
-        if (event.once) this.client.once(event.name, execute);
-        //@ts-ignore
-        else this.client.on(event.name, execute);
-    
-        delete require.cache[require.resolve(file)];
+      })
+    );
+  }
+
+  private registerCommand(command: Command | SubCommand, file: string): void {
+    if (!command.name) {
+      delete require.cache[require.resolve(file)];
+      console.log(`${file.split("/").pop()} doesn't have a name`);
+      return;
     }
 
-    async LoadCommands(): Promise<void> {
-        const files: string[] = (await glob('build/commands/**/*.js')).map(file => path.resolve(file));
-        
-        await Promise.all(files.map(async (file: string) => {
-            try {
-                const c: Command | SubCommand = new (await import(file)).default(this.client);
-                this.registerCommand(c, file);
-            } catch (error: any) {
-                this.client.logger.error(`Failed to load command from file ${file}: ${error.message}`, ["Handler", "LoadCommands"]);
-            }
-        }));
+    if (file.split("/").pop()?.split(".")[2]) {
+      this.client.subCommands.set(command.name, command);
+    } else {
+      this.client.commands.set(command.name, command as Command);
     }
-    
-    private registerCommand(command: Command | SubCommand, file: string): void {
-        if (!command.name) {
-            delete require.cache[require.resolve(file)];
-            console.log(`${file.split('/').pop()} doesn't have a name`);
-            return;
-        }
-    
-        if (file.split('/').pop()?.split('.')[2]) {
-            this.client.subCommands.set(command.name, command);
-        } else {
-            this.client.commands.set(command.name, command as Command);
-        }
-    
-        delete require.cache[require.resolve(file)];
-    }
+
+    delete require.cache[require.resolve(file)];
+  }
 }
